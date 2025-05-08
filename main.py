483d0e5f7b1ce678ea4b5ef48a21a0e79c510481
Ido Sar Shalom <idosarshalom@gmail.com>
2025-05-08 21:45:27 +0300

    first commit

--- main.py
+++ main.py
@@ -1,16 +1,78 @@
-# This is a sample Python script.
+from value_iteration import value_iteration
+from q_learning import q_learning
+from Grid import Grid
+from Uncertain_Grid import Uncertain_Grid
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+VI_POLICY_0 = {(0, 0): 'east', (0, 1): 'east', (0, 2): 'south', (1, 0): 'north',
+                    (1, 1): 'east', (1, 2): 'south', (2, 0): 'north'}
+
+VI_POLICY_1 = {(0, 0): 'east', (0, 1): 'east', (0, 2): 'south', (0, 3): 'south', (0, 4): 'south',
+               (1, 0): 'north', (1, 1): 'east', (1, 2): 'south', (1, 3): 'south', (1, 4): 'south',
+               (2, 2): 'east', (2, 3): 'south', (2, 4): 'west', (3, 0): 'east', (3, 1): 'south', (3, 2): 'south',
+               (3, 3): 'south', (3, 4): 'west', (4, 0): 'east', (4, 1): 'east', (4, 3): 'west', (4, 4): 'west'}
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Test one: Sanity check for value iteration
+def sanity_vi(print_policy=False):
+    problem_obj = Grid('routes_1.txt')
+    vi_policy = value_iteration(problem_obj)
+    print("Value iteration has finished running for problem 1, no compilation errors")
+    if print_policy:
+        problem_obj.pretty_print_policy(vi_policy)
+    if vi_policy == VI_POLICY_0:
+        print('Correct policy for sanity problem 1')
+    else:
+        print('Wrong policy for sanity problem 1')
+        print('Please check if all functions are completed (marked with TODO comments)')
+        print('Exiting sanity check for value iteration')
+        return False
+    problem_obj = Grid('routes_2.txt', grid_file=1)
+    vi_policy = value_iteration(problem_obj)
+    print(vi_policy)
+    print("Value iteration has finished running for problem 2, no compilation errors")
+    if print_policy:
+        problem_obj.pretty_print_policy(vi_policy)
+    if vi_policy == VI_POLICY_1:
+        print('Correct policy for sanity problem 2')
+        return True
+    else:
+        print('Wrong policy for sanity problem 2')
+        return False
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Test one: Sanity check for value iteration
+def sanity_ql(print_policy=False):
+    problem_obj = Uncertain_Grid('routes_1.txt')
+    ql_policy = q_learning(problem_obj)
+    print("Q Learning has finished running for problem 1, no compilation errors")
+    if print_policy:
+        problem_obj.pretty_print_policy(ql_policy)
+    if ql_policy == VI_POLICY_0:
+        print('Correct policy for sanity problem 1')
+    else:
+        print('Wrong policy for sanity problem 1')
+        print('Please check if all functions are completed (marked with TODO comments)')
+        print('Exiting sanity check for value iteration')
+        return False
+    problem_obj = Uncertain_Grid('routes_2.txt', grid_file=1)
+    ql_policy = q_learning(problem_obj, num_episodes=100000)
+    print("Q Learning has finished running for problem 2, no compilation errors")
+    if print_policy:
+        problem_obj.pretty_print_policy(ql_policy)
+    if ql_policy == VI_POLICY_1:
+        print('Correct policy for sanity problem 2')
+        return True
+    else:
+        print('Wrong policy for sanity problem 2')
+        return False
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+print("###------Testing value iteration------(You may update print_policy=False in order to not print the policy)###")
+vi_success = sanity_vi(print_policy=True)
+print("\n\n###------Testing Q learning------(You may update print_policy=False in order to not print the policy)###")
+ql_success = sanity_ql(print_policy=True)
+
+print("\n\n----Summary----")
+print("Value iteration success: ", vi_success)
+print("Q learning success: ", ql_success)
+
